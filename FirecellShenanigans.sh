020eedeb9396a4d90cf1ae988ff8dfd8aa0e27d6
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2022-08-04 14:34:17 -0700

    Update FirecellShenanigans.sh

--- FirecellShenanigans.sh
+++ FirecellShenanigans.sh
@@ -1,6 +1,6 @@
 cd ~/Documents
 git clone https://gitlab.com/firecell/r-d/firecellrd.git
-cd firecellrd
+cd ~/Documents/firecellrd
 export PATHTOREPO=$(pwd)
 export PATHTORAN=$PATHTOREPO/components/RAN
 export PATHTOEPC=$PATHTOREPO/components/CN/openair-epc
@@ -29,11 +29,13 @@ source oaienv
 
 # Run eNB
 cd $PATHTORAN/cmake_targets/
-sudo RFSIMULATOR=server ./ran_build/build/lte-softmodem -O ../ci- scripts/conf_files/enb.band7.tm1.25PRB.usrpb210.conf --noS1 -- nokrnmod 1 --rfsim
+sudo RFSIMULATOR=server ./ran_build/build/lte-softmodem -O ../ci-scripts/conf_files/enb.band7.tm1.25PRB.usrpb210.conf --noS1 --nokrnmod 1 --rfsim
+# -d --telnet_srv --rfsimulator.options chanmod
+
 
 # Run UE
 cd $PATHTORAN/cmake_targets/
-sudo RFSIMULATOR=<SERVERIP> ./ran_build/build/lte-uesoftmodem -C 2680000000 -r 25 --ue-rxgain 120 --ue-txgain 0 --ue-max-power 0 --ue- scan-carrier --nokrnmod 1 --noS1 --rfsim
+sudo RFSIMULATOR=<SERVERIP> ./ran_build/build/lte-uesoftmodem -C 2680000000 -r 25 --ue-rxgain 120 --ue-txgain 0 --ue-max-power 0 --ue-scan-carrier --nokrnmod 1 --noS1 --rfsim
 
 
 # RUN DIFFERENT NAME SPACES!!!!
@@ -51,6 +53,11 @@ cd $PATHTORAN/common/utils/T/tracer
 make
 ./macpdu2wireshark -d ../T_messages.txt -live -no-bind -ip 127.0.0.1
 
+# Don't forget to add the following line to the enb and ue configuration files
+@include "../ci-scripts/conf_files/channelmod_rfsimu.conf"
+
+
+################3
 ping -I oaitun_enb1 10.0.1.2  # (from eNB machine)
 ping -I oaitun_ue1 10.0.1.1   # (from UE machine)
 
