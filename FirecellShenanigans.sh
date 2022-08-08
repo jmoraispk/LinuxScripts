7b5f353619a6eb8a8c27fdf4908b34a1940c4b56
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2022-08-08 13:44:58 -0700

    Update FirecellShenanigans.sh

--- FirecellShenanigans.sh
+++ FirecellShenanigans.sh
@@ -211,7 +211,8 @@ sudo python3 ./core-network.py --type stop-basic --fqdn no --scenario 2
 
 
 
-# last commands
+# MY last commands IN 4G
+cd $PATHTORAN/cmake_targets/
 sudo RFSIMULATOR=server ./ran_build/build/lte-softmodem -O ../ci-scripts/conf_files/enb.band7.tm1.25PRB.usrpb210.conf --noS1 --nokrnmod 1 --rfsim -d --telnetsrv --rfsimulator.options chanmod
 sudo ./ran_build/build/lte-uesoftmodem -C 2680000000 -r 25 --ue-rxgain 120 --ue-txgain 0 --ue-max-power 0 --ue-scan-carrier --nokrnmod 1 --noS1 --rfsim --rfsimulator.serveraddr 10.0.1.1
 
@@ -232,17 +233,13 @@ sudo ip netns exec ue$ue_id ip link set dev lo up
 sudo ip netns exec ue$ue_id ip addr add 10.201.1.2/24 dev v-ue$ue_id
 sudo ip netns exec ue$ue_id ip link set v-ue$ue_id up
 
-sudo ip netns exec ue$ue_id sudo -E RFSIMULATOR=10.201.1.1 ./nr-uesoftmodem -r 106 --numerology 1 --band 78 -C 3619200000 --rfsim --sa --nokrnmod -O $CNFPATH/ue.conf
-sudo ./ran_build/build/nr-softmodem --rfsim --sa -d -O ../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.fr1.106PRB.usrpb210.conf
-sudo ./ran_build/build/nr-softmodem --rfsim --sa -d -O ../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.fr1.106PRB.usrpb210.conf
-export RFSIMULATOR=server
+
 
 
 
 # Final commands
 sudo ip netns exec ue$ue_id sudo -E RFSIMULATOR=10.201.1.1 ./ran_build/build/nr-uesoftmodem -r 106 --numerology 1 --band 78 -C 3619200000 --rfsim --sa --noS1 --nokrnmod -O ue.conf
-sudo ./ran_build/build/nr-softmodem --rfsim --sa --noS1 --nokrnmod -d -O gnb.sa.band78.fr1.106PRB.usrpb210.conf
-
+sudo RFSIMULATOR=server ./ran_build/build/nr-softmodem --rfsim --sa --noS1 --nokrnmod -d -O gnb.sa.band78.fr1.106PRB.usrpb210.conf
 
 ################3
 ping -I oaitun_enb1 10.0.1.2  # (from eNB machine)
